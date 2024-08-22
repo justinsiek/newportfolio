98f676fb7d2eae554e1a4e5fe97533388ef8e6ef
Justin Siek <justinsiek05@gmail.com>
2024-08-21 22:01:49 -0700

    complete initial page

---
+++ components/cursor.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+
+const Cursor = () => {
+
+  const [cursorPosition, setCursorPosition] = React.useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      setCursorPosition({ x: e.clientX, y: e.clientY });
+    }
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }
+  , []);
+
+  return (
+    <div className='fixed pointer-events-none w-6 h-6 rounded-full bg-white mix-blend-difference z-50' 
+        style={{
+          left: cursorPosition.x,
+          top: cursorPosition.y,
+          transform: 'translate(-50%, -50%)',
+    }}/>
+  )
+}
+
+export default Cursor
